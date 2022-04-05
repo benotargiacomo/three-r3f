9d189914b09a6a0d8f5edd68a1ce6e0d499979eb
Bernardo <benotargiacomo@gmail.com>
2022-04-05 14:59:04 -0300

    Create GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,31 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { softShadows, OrbitControls } from '@react-three/drei';
 
+import GlobalStyles from './styles/GlobalStyles';
+
 softShadows();
 
 function Box() {
   return (
     <mesh>
-      <boxBufferGeometry attach="geometry" />
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshStandardMaterial attach="material" color="red" />
     </mesh>
   );
 }
 
-export default function App() {
+function App() {
   return (
-    <Canvas>
-      <OrbitControls />
-      <ambientLight intensity={0.2} />
-      <spotLight position={[20, 15, 20]} angle={0.2} />
-      <Box />
-    </Canvas>
+    <>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={0.2} />
+        <spotLight position={[20, 15, 20]} angle={0.2} />
+        <Box />
+      </Canvas>
+      <GlobalStyles />
+    </>
   );
 }
+
+export default App;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    max-height: 100vh;
+    height: 100%;
+    background-color: #747572;
+  }
+  
+  *, button, input {
+    border: 0;
+    background-color: none;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    outline: 0;
+  }
+`;
