c513df3eddfe8ec90d7e04d250db24bbef2f4033
Bernardo <benotargiacomo@gmail.com>
2022-04-05 09:36:40 -0300

    Add @react-three/drei

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { softShadows, OrbitControls } from '@react-three/drei';
+
+softShadows();
 
 export default function App() {
   return (
-    <h1>Setup</h1>
+    <Canvas>
+      <OrbitControls />
+    </Canvas>
   );
 }
