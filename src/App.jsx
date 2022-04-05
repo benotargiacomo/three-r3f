a5bf54b6964075088c56837970186cbedfe0c306
Bernardo <benotargiacomo@gmail.com>
2022-04-05 14:05:56 -0300

    Add simple object to test render

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import { softShadows, OrbitControls } from '@react-three/drei';
 
 softShadows();
 
+function Box() {
+  return (
+    <mesh>
+      <boxBufferGeometry attach="geometry" />
+      <meshStandardMaterial attach="material" color="red" />
+    </mesh>
+  );
+}
+
 export default function App() {
   return (
     <Canvas>
       <OrbitControls />
+      <ambientLight intensity={0.2} />
+      <spotLight position={[20, 15, 20]} angle={0.2} />
+      <Box />
     </Canvas>
   );
 }
