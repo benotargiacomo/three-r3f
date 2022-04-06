23341ccd8e4df7df6e1be847f49f83664a1e5e8c
Bernardo <benotargiacomo@gmail.com>
2022-04-06 12:21:22 -0300

    Test some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { softShadows, OrbitControls } from '@react-three/drei';
+import {
+  softShadows,
+  // PresentationControls,
+  OrbitControls,
+  // Environment,
+  // ContactShadows,
+} from '@react-three/drei';
 
 import CarModel from './components/CarModel';
 import Floor from './components/Floor';
@@ -14,10 +20,13 @@ function App() {
     <>
       <Canvas
         shadows
+        dpr={[1, 2]}
         camera={{ position: [3, 1.5, 4], fov: 45 }}
         className="canvas"
       >
-        <ambientLight intensity={0.3} />
+        <color attach="background" args={['#181818']} />
+        <fog attach="fog" args={['#181818', 10, 20]} />
+        {/* <ambientLight intensity={0.3} /> */}
         <directionalLight
           castShadow
           intensity={0.8}
@@ -31,11 +40,33 @@ function App() {
           shadow-camera-top={10}
           shadow-camera-bottom={-10}
         />
+        {/* <PresentationControls polar={[0, 0]} config={{ mass: 1.3, tension: 500 }}> */}
         <Suspense>
-          <CarModel />
+          <group scale={0.8}>
+            <CarModel />
+            <Floor />
+          </group>
         </Suspense>
-        <Floor />
-        <OrbitControls />
+        {/* </PresentationControls> */}
+        {/* <ContactShadows
+          resolution={2048}
+          frames={1}
+          position={[0, -1.16, 0]}
+          scale={10}
+          blur={0.75}
+          opacity={1}
+          far={10}
+        /> */}
+        {/* <Environment preset="warehouse" /> */}
+        <OrbitControls
+          rotateSpeed={0.2}
+          autoRotate
+          autoRotateSpeed={0.2}
+          enablePan={false}
+          enableZoom={false}
+          minPolarAngle={Math.PI / 2.4}
+          maxPolarAngle={Math.PI / 2.4}
+        />
       </Canvas>
       <GlobalStyles />
     </>
--- src/components/Floor.jsx
+++ src/components/Floor.jsx
@@ -1,10 +1,22 @@
+import { MeshReflectorMaterial } from '@react-three/drei';
 import React from 'react';
 
 function Floor() {
   return (
     <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[5, 7]} />
-      <shadowMaterial attach="material" opacity={1} />
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <MeshReflectorMaterial
+        blur={[500, 500]}
+        resolution={1024}
+        mixBlur={0.5}
+        mixStrength={20}
+        roughness={1}
+        depthScale={1.2}
+        minDepthThreshold={0.4}
+        maxDepthThreshold={1.4}
+        color="#181818"
+        metalness={0.5}
+      />
     </mesh>
   );
 }
